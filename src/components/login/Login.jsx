b290e8c5edeac94faae6705d8981352038652fce
Aleksandr Kabanskiy <alexander.777.kr@gmail.com>
2021-03-07 21:34:35 +0300

    Fixed getAuthUserData action creator

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,73 +1,77 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { required } from '../../utilites/validators/validators';
-import { Input } from '../common/formControls/FormControls';
-import classes from './Login.module.css';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { required } from "../../utilites/validators/validators";
+import { Input } from "../common/formControls/FormControls";
+import classes from "./Login.module.css";
 import { connect } from "react-redux";
-import {login} from '../redux/authReducer';
-import { Redirect } from 'react-router-dom';
+import { login } from "../redux/authReducer";
+import { Redirect } from "react-router-dom";
 
 const LoginForm = (props) => {
-    return (
-<div >
-    
-    <form onSubmit = {props.handleSubmit} className = {classes.loginForm} >
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit} className={classes.loginForm}>
         <div>
-        
-            <Field 
-        component = {Input} 
-        name = {'email'} 
-        placeholder = {'Enter email'} 
-        type={"text"} 
-        className = {classes.formText}
-        validate = {[required]}/></div>
+          <Field
+            component={Input}
+            name={"email"}
+            placeholder={"Enter email"}
+            type={"text"}
+            className={classes.formText}
+            validate={[required]}
+          />
+        </div>
         <div>
-
-            <Field 
-        component = {Input} 
-        name = {'password'} 
-        placeholder = {'Enter password'} 
-        type={"password"} 
-        className = {classes.formPassword}
-        validate = {[required]}/>
+          <Field
+            component={Input}
+            name={"password"}
+            placeholder={"Enter password"}
+            type={"password"}
+            className={classes.formPassword}
+            validate={[required]}
+          />
         </div>
 
-        <div >
-            <Field component = {Input} name = {'rememberMe'} type={'checkbox'} className = {classes.formCheckbox} /><span>Remember me</span></div> 
         <div>
-        {props.error && <div className ={classes.someError}>
-            {props.error}
-    
-    </div>}
-            <button className = "greenBtn">Login</button></div>
-    </form>
-</div>
-    )
-}
+          <Field
+            component={Input}
+            name={"rememberMe"}
+            type={"checkbox"}
+            className={classes.formCheckbox}
+          />
+          <span>Remember me</span>
+        </div>
+        <div>
+          {props.error && (
+            <div className={classes.someError}>{props.error}</div>
+          )}
+          <button className="greenBtn">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
 const Login = (props) => {
+  const onSubmit = (formData) => {
+    props.login(formData.email, formData.password, formData.rememberMe);
+  };
 
-    const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe);
-
-    }
-
-    if(props.isAuth) {
-        return <Redirect to = {'/profile'} />
-    }
-    return (
-<div className = {classes.formContainer}>
-    <h3>Login</h3>
-    <LoginReduxForm onSubmit = {onSubmit}/>
-</div>
-    )
-}
+  if (props.isAuth) {
+    return <Redirect to={"/profile"} />;
+  }
+  return (
+    <div className={classes.formContainer}>
+      <h3>Login</h3>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth
+  isAuth: state.authPage.isAuth,
+});
 
-})
-
-export default connect(mapStateToProps, { login})(Login);
+export default connect(mapStateToProps, { login })(Login);
--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -2,48 +2,46 @@ import React from "react";
 import Profile from "./Profile";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import {getUserProfile, getStatus, updateStatus } from "../redux/profileReducer";
+import {
+  getUserProfile,
+  getStatus,
+  updateStatus,
+} from "../redux/profileReducer";
 import { compose } from "redux";
 import { withAuthRedirect } from "../../hoc/withAuthRedirect";
- 
-
-
 
 class ProfileContainer extends React.Component {
-    
-    componentDidMount () {
+  componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = this.props.authorizedUserId;
+    }
 
-        let userId = this.props.match.params.userId;
-        if(!userId){
+    this.props.getUserProfile(userId);
 
-            userId = this.props.authorizedUserId;
-        }
-
-        this.props.getUserProfile(userId);
-        
-            this.props.getStatus(userId);
-        }
-    render(){
+    this.props.getStatus(userId);
+  }
+  render() {
+    console.log(this.props);
     return (
-        <Profile {...this.props} 
-        profile = {this.props.profile} 
-        status = {this.props.status} 
-        updateStatus = {this.props.updateStatus}/>
-    )
+      <Profile
+        {...this.props}
+        profile={this.props.profile}
+        status={this.props.status}
+        updateStatus={this.props.updateStatus}
+      />
+    );
+  }
 }
-}
-
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile,
-    status: state.profilePage.status,
-    authorizedUserId: state.authPage.userId,
-    isAuth: state.authPage.isAuth
-    
+  profile: state.profilePage.profile,
+  status: state.profilePage.status,
+  authorizedUserId: state.authPage.userId,
+  isAuth: state.authPage.isAuth,
 });
 
-
 export default compose(
-    connect (mapStateToProps, {getUserProfile, getStatus, updateStatus}),
-    withRouter
+  connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
+  withRouter
 )(ProfileContainer);
